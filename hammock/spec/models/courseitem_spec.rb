64d3febbaa017671965488b8ba55f3f782c0889f
Marion LV <marion.levisage@gmail.com>
2016-03-23 11:29:00 +0000

    Installed VCR gem and attempting test with it

--- hammock/spec/models/courseitem_spec.rb
+++ hammock/spec/models/courseitem_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe Courseitem, type: :model do
-  xit 'queries Udacity for courses' do
-    uri = URI('https://www.udacity.com/public-api/v0/courses')
-    expect().to be_an_instance_of(Hash)
+  context "Udacity" do
+    it 'makes a successful call to the API' do
+          VCR.use_cassette("udacity_api") do
+            response = Net::HTTP.get_response(URI('https://www.udacity.com/public-api/v0/courses'))
+            assert_match /Example domains/, response.body
+          end
+
+      expect(response).to be_an_instance_of(String)
+    end
+
+    xit 'checks that the database is populate with Udacity API data' do
+      expect
+    end
   end
 end
--- hammock/spec/rails_helper.rb
+++ hammock/spec/rails_helper.rb
@@ -20,4 +20,11 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock # or :fakeweb
+  c.allow_http_connections_when_no_cassette = true
 end
