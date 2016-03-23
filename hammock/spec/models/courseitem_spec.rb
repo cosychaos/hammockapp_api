9d73f6fffc0b7c82b70275781f1e233194a610aa
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-03-23 11:55:26 +0000

    api test

--- hammock/spec/models/courseitem_spec.rb
+++ hammock/spec/models/courseitem_spec.rb
@@ -2,18 +2,13 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe Courseitem, type: :model do
-  context "Udacity" do
+  describe "Udacity", :vcr do
     it 'makes a successful call to the API' do
-          VCR.use_cassette("udacity_api") do
-            response = Net::HTTP.get_response(URI('https://www.udacity.com/public-api/v0/courses'))
-            assert_match /Example domains/, response.body
-          end
-
-      expect(response).to be_an_instance_of(String)
+      expect(Courseitem.request_udacity_courses).to be_an_instance_of(Hash)
     end
 
-    xit 'checks that the database is populate with Udacity API data' do
-      expect
+    it 'checks that the database is populate with Udacity API data' do
+      expect{Courseitem.add_udacity_courses}.to change {Courseitem.count}
     end
   end
 end
