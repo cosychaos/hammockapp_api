98cae30afbf433c3a4503884fb8b9776bfd9c19c
Marion LV <marion.levisage@gmail.com>
2016-03-23 15:49:45 +0000

    Tried to populate database

--- hammock/app/models/courseitem.rb
+++ hammock/app/models/courseitem.rb
@@ -1,2 +1,35 @@
 class Courseitem < ActiveRecord::Base
+
+  # FIELDS = {
+  # provider:
+  # name:
+  # description:
+  # organisation:
+  # image:
+  # url:
+  # duration:
+  # startdate:
+  # enddate:
+  # }
+
+  def self.request_udacity_courses
+    uri = URI.parse("https://www.udacity.com/public-api/v0/courses")
+
+    response = Net::HTTP.get_response(uri)
+
+    JSON.parse(response.body)["courses"]
+
+  end
+
+  def self.add_udacity_courses
+
+    response = self.request_udacity_courses
+
+    response.each do |course|
+      a = Courseitem.new(provider: 'Udacity', name: course["title"], description: course["short_summary"])
+      a.save
+    end
+
+  end
+
 end
--- hammock/spec/models/courseitem_spec.rb
+++ hammock/spec/models/courseitem_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe Courseitem, type: :model do
   describe "Udacity", :vcr do
     it 'makes a successful call to the API' do
-      expect(Courseitem.request_udacity_courses).to be_an_instance_of(Hash)
+      expect(Courseitem.request_udacity_courses).to be_an_instance_of(Array)
     end
 
     it 'checks that the database is populate with Udacity API data' do
