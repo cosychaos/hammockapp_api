8e02ceac1fadf83259f8f519f74aafcd75033ef9
Marion LV <marion.levisage@gmail.com>
2016-03-24 17:02:09 +0000

    Tested that courses are returned (or not) when user is logged in (or not)

--- hammock/app/controllers/courses_controller.rb
+++ hammock/app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
-    render json: Course.all
+    render json: Course.where(user_id: current_user.id)
   end
 end
--- hammock/db/schema.rb
+++ hammock/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322152512) do
+ActiveRecord::Schema.define(version: 20160324160522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,10 @@ ActiveRecord::Schema.define(version: 20160322152512) do
     t.datetime "enddate"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
@@ -73,4 +76,5 @@ ActiveRecord::Schema.define(version: 20160322152512) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
+  add_foreign_key "courses", "users"
 end
