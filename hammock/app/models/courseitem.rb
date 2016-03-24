a66f02fa13612770bfb9cde7dd0b4598e3d8af49
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-03-24 12:07:12 +0000

    single request to api

--- hammock/app/models/courseitem.rb
+++ hammock/app/models/courseitem.rb
@@ -13,4 +13,26 @@ class Courseitem < ActiveRecord::Base
       a.save
     end
   end
+
+  def self.request_coursera_courses(start)
+    uri = URI.parse("https://api.coursera.org/api/courses.v1/?fields=photoUrl,description,slug" + start)
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)["elements"]
+  end
+
+  def self.add_coursera_courses
+    response = self.request_coursera_courses
+    response.each do |course|
+      a = Courseitem.new(provider: 'Coursera', name: course["name"], description: course["description"], organisation: "Coursera", image: course["photoUrl"], url: create_coursera_url(course["slug"], course["courseType"]))
+      a.save
+    end
+  end
+
+
+  private
+  def self.create_coursera_url(slug, course_type)
+    return "https://www.coursera.org/course/" + slug if course_type == "v1.session"
+    return "https://www.coursera.org/learn/" + slug if course_type == "v2.ondemand"
+  end
+
 end
