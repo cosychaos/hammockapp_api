9225fb3334df1520b2ed39f508e0d40149e93bed
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-03-24 09:57:11 +0000

    udacity api setup

--- hammock/spec/models/courseitem_spec.rb
+++ hammock/spec/models/courseitem_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe Courseitem, type: :model do
-  describe "Udacity", :vcr do
+describe "Course database", type: :model do
+  context "Udacity" do
     it 'makes a successful call to the API' do
-      expect(Courseitem.request_udacity_courses).to be_an_instance_of(Array)
+      expect(Courseitem.request_udacity_courses[0].has_key?("title")).to eq true
     end
 
-    it 'checks that the database is populate with Udacity API data' do
-      expect{Courseitem.add_udacity_courses}.to change {Courseitem.count}
+    it 'checks that the database is populated with Udacity API data' do
+      Courseitem.add_udacity_courses
+      expect(Courseitem.where(name: "IntrotoComputerScience")).to exist
     end
   end
 end
