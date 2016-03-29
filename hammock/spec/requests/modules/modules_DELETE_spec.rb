8c7bde26e86b3c49cf37ae946b0ff2d0c02b766a
esbaddeley <esbaddeley@gmail.com>
2016-03-29 11:32:47 +0100

    Added modules DELETE route

--- hammock/spec/requests/modules/modules_DELETE_spec.rb
+++ hammock/spec/requests/modules/modules_DELETE_spec.rb
@@ -1,8 +1,8 @@
 describe 'Modules API' do
 
-  describe 'PUT /course_modules/:id' do
+  describe 'DELETE /course_modules/:id' do
 
-    it "updates a course" do
+    it "deletes a course_module" do
       user = FactoryGirl.create :user
       course = FactoryGirl.build :course
       course.user_id = user.id
@@ -19,10 +19,9 @@ describe 'Modules API' do
           "complete": true
         }
       }.to_json
-      put "/course_modules/#{course_module.id}", course_module_params, auth_headers
-      expect(response.status).to eq 202
-      expect(CourseModule.last.id).to eq course_module.id
-      expect(CourseModule.last.complete).to eq true
+      delete "/course_modules/#{course_module.id}", course_module_params, auth_headers
+      expect(response.status).to eq 204
+      expect(CourseModule.exists?(course_module.id)).to eq false
     end
 
   end
